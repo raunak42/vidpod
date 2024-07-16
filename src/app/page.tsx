909b71c7f3dc2303917676a897d46ea48dac2ebb
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-16 12:03:32 +0530

    Main front page structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
 import TodoList from "./_components/todoList";
+import { Navbar } from "./_components/Navbar";
+import { Sidebar } from "./_components/Sidebar";
 
 export default function Home() {
   return (
     <div>
-      <TodoList />
+      <div className="flex flex-col">
+        <Navbar />
+        <Sidebar/>
+      </div>
     </div>
   );
 }
