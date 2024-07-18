32f0b4214f746ae4d407f97676913b0e5299a900
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-18 08:00:09 +0530

    Wavecomponent created with wavesurfer.

---
+++ src/app/_components/WaveComponent.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { videoLengthState } from "@/store/videoState";
+import { useEffect, useRef, useState } from "react";
+import { useRecoilState } from "recoil";
+import WaveSurfer from "wavesurfer.js";
+
+interface WaveComponentProps {
+  audioFile: string; 
+}
+
+export const WaveComponent: React.FC<WaveComponentProps> = ({
+  audioFile,
+}) => {
+  const waveFormRef = useRef<HTMLDivElement>(null);
+  const wavesurferRef = useRef<WaveSurfer | null>(null);
+  const [videoLengthSeconds, setVideoLengthSeconds] =
+    useRecoilState(videoLengthState);
+  const timeLineLength = videoLengthSeconds * 1.5;
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (waveFormRef.current && !wavesurferRef.current) {
+      wavesurferRef.current = WaveSurfer.create({
+        container: waveFormRef.current,
+        waveColor: "#ffffff", 
+        progressColor: "#ffffff", 
+        barWidth: 1, 
+        barHeight: 1, 
+        height: 100, 
+        barGap: 2,
+      });
+    }
+
+    let isMounted = true;
+
+    if (wavesurferRef.current && audioFile) {
+      wavesurferRef.current.load(audioFile).then(() => {
+        if (isMounted) {
+          setIsLoaded(true);
+        }
+      });
+    }
+
+    return () => {
+      isMounted = false;
+      if (wavesurferRef.current && isLoaded) {
+        wavesurferRef.current.destroy();
+      }
+    };
+  }, [audioFile, isLoaded])
+
+  return (
+    <div>
+      <div ref={waveFormRef} style={{ width: `${timeLineLength}px`, height: "100px" }} />
+    </div>
+  );
+};
