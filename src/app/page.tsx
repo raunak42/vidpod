6d195724b1baa68f45b50b891d1113c4a789ffdb
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-19 11:30:54 +0530

    Layout changes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,14 @@ import { VideoCard } from "./_components/VideoCard";
 import { Footer } from "./_components/Footer";
 import { Timeline } from "./_components/Timeline";
 import { Haze } from "./_components/Haze";
+import Image from "next/image";
+import { TimelineButtons } from "./_components/TimelineButtons";
 
 export default function Home() {
   return (
     <div className="relative">
       {/* <div className="h-[100vh] w-full z-20 opacity-45  bg-red-200 absolute"> */}
-        <Haze />
+      <Haze />
       {/* </div> */}
 
       <div className="flex flex-col">
@@ -20,14 +22,15 @@ export default function Home() {
         <div className="flex flex-row">
           <Sidebar />
 
-          <div className="w-full flex flex-col p-[64px] gap-[32px]">
+          <div className="w-full flex flex-col p-[32px] gap-[32px]">
             <EpisodeDescription />
             <div className="flex flex-row justify-between gap-[32px]">
               <AdMarkersCard />
               <VideoCard />
             </div>
 
-            <div className=" flex flex-col w-[960px] h-[358px] border-[1px] border-[#E4E4E7] rounded-[16px] shadow-sm p-[32px] ">
+            <div className=" flex flex-col w-[1040px] h-[358px] border-[1px] border-[#E4E4E7] rounded-[16px] shadow-sm p-[32px] ">
+              <TimelineButtons/>
               <Timeline />
             </div>
           </div>
