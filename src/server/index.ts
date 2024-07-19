e50189729df7807a676928225efb3dfc07b11024
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-19 11:28:57 +0530

    Api routes for crud operations.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,80 @@
-import { db } from '@/lib/prisma';
 import { publicProcedure, router } from './trpc';
-import { z } from "zod"
+import { z } from 'zod';
+import { db } from '@/lib/prisma';
+
+const AdSchema = z.object({
+    length: z.number(),
+    name: z.string(),
+    company: z.string(),
+    coverImage: z.string(),
+    videoUrl: z.string(),
+    publishedDate: z.string(),
+});
+
 
 export const appRouter = router({
-    getTodos: publicProcedure.query(async () => {
-        return [10, 20, 30, 40]
+    getMarkers: publicProcedure.query(async () => {
+        const markers = await db.adMarker.findMany()
+        return markers;
     }),
 
-    getDick: publicProcedure.query(async () => {
-        return "DICKMEUP"
+    newMarker: publicProcedure
+        .input(z.object({
+            adType: z.enum(["Auto", "Static", "A/B test", ""]),
+            adStarts: z.number(),
+            ads: z.array(AdSchema).optional(),
+        }))
+        .mutation(async ({ input }) => {
+            await db.adMarker.create({
+                data: {
+                    markerStart: input.adStarts,
+                    markerType: input.adType,
+                    ads: input.ads ? {
+                        create: input.ads.map(ad => ({
+                            length: ad.length,
+                            name: ad.name,
+                            company: ad.company,
+                            coverImage: ad.coverImage,
+                            videoUrl: ad.videoUrl,
+                            publishedDate: ad.publishedDate,
+                        }))
+                    } : undefined
+                }
+            })
+            console.log(JSON.stringify(input));
+            return { success: true, message: "Success" };
+        }),
+
+    deleteMarker: publicProcedure
+        .input(z.number())
+        .mutation(async ({ input }) => {
+            await db.adMarker.delete({
+                where: {
+                    id: input
+                }
+            })
+        }),
+
+    editMarker: publicProcedure
+        .input(z.object({
+            markerId: z.number(), newStart: z.number(), adType: z.enum(["Auto", "Static", "A/B test", ""]),
+        }))
+        .mutation(async ({ input }) => {
+            await db.adMarker.update({
+                where: {
+                    id: input.markerId
+                },
+                data: {
+                    markerStart: input.newStart,
+                    markerType: input.adType
+                }
+            })
+        }),
+
+    getAds: publicProcedure.query(async () => {
+        const ads = await db.ad.findMany()
+        return ads;
     })
 });
 export type AppRouter = typeof appRouter;
+
