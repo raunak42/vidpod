7f38e8533d6590c39ea21322e5a4b7f5df5804d4
Raunak Lanjewar <raunaklanjewar42@gmail.com>
2024-07-19 11:33:08 +0530

    Minor changes.

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar: React.FC = () => {
     <div>
       {/* <TodoList /> */}
       <div className="w-full flex flex-row px-[64px] py-[24px] justify-between items-center border-b-[1px] border-b-[#E4E4E7]">
-        <div className="flex flex-row gap-[16px]">
+        <button className="flex flex-row gap-[16px]">
           <Image
             alt="brandmark"
             src={"/brandmark.svg"}
@@ -13,29 +13,34 @@ export const Navbar: React.FC = () => {
             width={23.04}
           ></Image>
           <h1 className="text-[24px] font-[700] leading-[32px]">Vidpod</h1>
-        </div>
+        </button>
 
         <div className="flex flex-row gap-[32px]">
-          <Image
-            alt="settings"
-            src={"/settings.svg"}
-            height={20}
-            width={20}
-          ></Image>
+          <button>
+            <Image
+              alt="settings"
+              src={"/settings.svg"}
+              height={20}
+              width={20}
+            ></Image>
+          </button>
+          <button>
+            <Image
+              alt="bell-dot"
+              src={"/bell-dot.svg"}
+              height={20}
+              width={20}
+            ></Image>
+          </button>
 
-          <Image
-            alt="bell-dot"
-            src={"/bell-dot.svg"}
-            height={20}
-            width={20}
-          ></Image>
-
-          <div className="w-[206px] h-[56px] border-[1px] border-[#E4E4E7] rounded-[8px] flex flex-row justify-between items-center px-[16px]">
+          <button className="w-[206px] h-[56px] border-[1px] border-[#E4E4E7] rounded-[8px] flex flex-row justify-between items-center px-[16px]">
             <div className="size-[32px] overflow-hidden rounded-full flex">
               <img src="/image.png" className="object-cover size-full"></img>
             </div>
 
-            <h1 className="text-[16px] font-[700] leading-[24px] text-[#27272A]">Emma Warren</h1>
+            <h1 className="text-[16px] font-[700] leading-[24px] text-[#27272A]">
+              Emma Warren
+            </h1>
 
             <Image
               alt="chevron-down"
@@ -43,7 +48,7 @@ export const Navbar: React.FC = () => {
               height={16}
               width={16}
             ></Image>
-          </div>
+          </button>
         </div>
       </div>
     </div>
